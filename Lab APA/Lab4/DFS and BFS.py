9ec37e18f04ae4c904a1e6fa1d0483c8f7e44c6b
andeiceban0352 <92468173+andeiceban0352@users.noreply.github.com>
2023-04-13 13:03:02 +0300

    sd

--- Lab APA/Lab4/DFS and BFS.py
+++ Lab APA/Lab4/DFS and BFS.py
@@ -1,66 +1,64 @@
-import networkx as complex_graph
 import matplotlib.pyplot as plt
-import time
+from graphviz import Digraph
+# Define the TreeNode class and the BFS and DFS functions
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-def BFS(graph, first_node):
-    visited = []  # Create an array for visited nodes
-    queue = [first_node]  # Create a queue for BFS
+@time_it
+def BFS_contains(root, target):
+    queue = [root]  # Create a queue for BFS
     while queue:
         node = queue.pop(0) #dequeue the vertex from queue
-        if node not in visited:
-            visited.append(node) # put the node in the visited list
-            # Filter out the nodes that have already been visited
-            not_visited = lambda n: n not in visited
-            unvisited_nodes = filter(not_visited, graph.neighbors(node))
-            # Add the unvisited nodes to the queue
-            queue.extend(unvisited_nodes)
-    return visited
+        if node:
+            if node.val == target:
+                return True
+            # Add the child nodes to the queue
+            queue.append(node.left)
+            queue.append(node.right)
+    return False
 
-def DFS(graph, start_node):
-    visited = [] # store the value of the visited nodes
-    stack = [start_node] # Create a stack for DFS
-    while stack:
-        node = stack.pop() #dequeue the vertex from stack
-        if node not in visited:
-            visited.append(node) # put the node in the visited list
-            # Filter out the nodes that have already been visited
-            not_visited = lambda n: n not in visited
-            unvisited_nodes = filter(not_visited, graph.neighbors(node))
-            # Add the unvisited nodes to the queue
-            stack.extend(unvisited_nodes)
-    return visited
+@time_it
+def DFS_contains(root, target):
+    if not root:
+        return False
+    if root.val == target:
+        return True
+    return DFS_contains(root.left, target) or DFS_contains(root.right, target)
 
+def print_tree(node):
+    dot = Digraph()
 
-DFS_time = []  #store time execution for DFS
-BFS_time = []  #store time execution for BFS
-node_counts = [] # store nodes
+    def add_node(node):
+        if node:
+            dot.node(str(node.val))
+            if node.left:
+                dot.edge(str(node.val), str(node.left.val))
+                add_node(node.left)
+            if node.right:
+                dot.edge(str(node.val), str(node.right.val))
+                add_node(node.right)
 
-for n in range(15, 700 , 10):
-    node_counts.append(n)
-    graph = complex_graph.gnp_random_graph(n, 0.1)
+    add_node(node)
+    dot.render('binary_trees.gv', view=True)
 
-    start_time = time.time()
-    DFS(graph, 0)
-    end_time = time.time()
-    DFS_time.append(end_time - start_time)
+root = list_to_bt([i for i in range(30)])
+print_tree(root)# Plot the time taken by both BFS and DFS algorithms to search for each node in the tree
+fig, ax = plt.subplots(figsize=(20, 6))
 
-    start_time = time.time()
-    BFS(graph, 0)
-    end_time = time.time()
-    BFS_time.append(end_time - start_time)
+bfs_times = []
+dfs_times = []
 
-plt.bar(node_counts, BFS_time, label="BFS", )
-plt.xlabel('Number of nodes')
-plt.ylabel('Execution time (seconds)')
-plt.title('BFS Algorithm time execution')
-plt.legend()
-plt.show()
+for i in range(31):
+    bfs_times.append(BFS_contains(root, i)[0])
+    dfs_times.append(DFS_contains(root, i)[0])
 
-
-plt.bar(node_counts, DFS_time, label="DFS", )
-plt.xlabel('Number of nodes')
-plt.ylabel('Execution time (seconds)')
-plt.title('DFS Algorithm time execution')
-plt.legend()
+ax.bar(range(31), bfs_times, label="BFS")
+ax.bar(range(31), dfs_times, bottom=bfs_times, label="DFS")
+ax.set_title("BFS vs DFS")
+ax.legend()
+plt.xticks(range(31))
 plt.show()
